a1858c38a0a7f85c44040c03097a51b0f3e57de7
unknown <rajaanthonysamy5798@gmail.com>
2021-01-18 20:46:47 +0530

    simple modified by nithin

--- Simple.java
+++ Simple.java
@@ -1,5 +1,7 @@
 class Simple{  
-    public static void main(String args[]){  
+    public static void main(String args[]){ 
+     String name="Raja";
+     System.outprintln(name); 
      System.out.println("Nithin update");  
     }  
 }  
